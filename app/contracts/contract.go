7329b1e54e039bec1c7779193c7f5e91e008da84
wesleywcr <wesley.rodrigues.wcr@gmail.com>
2026-04-02 10:32:35 -0300

    feat: add simpleStorageABI is the JSON ABI for the SimpleStorage contract

---
+++ app/contracts/contract.go
@@ -0,0 +1,4 @@
+package contracts
+
+// SimpleStorageABI is the JSON ABI for the SimpleStorage contract (see Foundry out artifact).
+const SimpleStorageABI = "[{\"inputs\":[{\"internalType\":\"uint256\",\"name\":\"x\",\"type\":\"uint256\"}],\"name\":\"set\",\"outputs\":[],\"stateMutability\":\"nonpayable\",\"type\":\"function\"},{\"inputs\":[],\"name\":\"get\",\"outputs\":[{\"internalType\":\"uint256\",\"name\":\"\",\"type\":\"uint256\"}],\"stateMutability\":\"view\",\"type\":\"function\"},{\"inputs\":[],\"name\":\"storedData\",\"outputs\":[{\"internalType\":\"uint256\",\"name\":\"\",\"type\":\"uint256\"}],\"stateMutability\":\"view\",\"type\":\"function\"}]"
